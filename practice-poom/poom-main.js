b29d4a6cbd41b4e5f1f39bd4ecc74320bab13a15
poomamorn <machalepoom@hotmail.com>
2016-10-28 17:05:46 +0700

    poom change folder name from poom-practice to practice-poom

--- practice-poom/poom-main.js
+++ practice-poom/poom-main.js
@@ -12,6 +12,8 @@ app.get('/',function(req,res){
 });
 app.use(express.static(__dirname+'/public'));
 
+
+
 app.listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
